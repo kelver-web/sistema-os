48caefe522462b692d4f5f853b90146a1865c958
kelver-web <kelverwt@gmail.com>
2026-07-26 11:59:25 -0300

    feat: Cria Part e PartMovement models

--- parts/models.py
+++ parts/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Part(models.Model):
+    name = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200, blank=True)
+    model_number = models.CharField(max_length=100, blank=True)
+    supplier = models.CharField(max_length=200, blank=True)
+    supplier_price = models.FloatField()
+    sale_price = models.FloatField()
+    quantity = models.PositiveIntegerField()
+    location = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
+    
+
+class PartMovement(models.Model):
+    class MovementType(models.TextChoices):
+        IN = 'in', 'Entrada'
+        USED = 'used', 'Usado'
+    
+    part = models.ForeignKey(
+        Part, on_delete=models.PROTECT, related_name='movements'
+    )
+    service_order = models.ForeignKey(
+        'service_orders.ServiceOrder', on_delete=models.SET_NULL, related_name='part_movements',
+        null=True, blank=True
+    )
+    movement_type = models.CharField(max_length=10, choices=MovementType.choices)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.FloatField()
+    notes = models.TextField(blank=True)
+    created_by = models.ForeignKey(
+        'accounts.User', on_delete=models.PROTECT
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f'{self.movement_type} - {self.part.name}'
