7c2b6408146f2cb52c6bb0fbc1fe28553000089d
kelver-web <kelverwt@gmail.com>
2026-08-10 18:41:07 -0300

    feat: configura autenticação JWT e endpoint /me/ muda urls de login app accounts

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from django.urls import path
+from accounts.views import MeView
+
+
+urlpatterns = [
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("auth/me/", MeView.as_view(), name="me"),
+]
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -15,9 +10,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("clients.urls")),
     path("api/", include("equipments.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/", include("accounts.urls")),
     path(
         "api/schema/",
         SpectacularAPIView.as_view(),
